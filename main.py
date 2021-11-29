2983301ce8acb75ca0b5d7c7e2344deffce1b7b3
yeahrighttv <martinjansander@gmail.com>
2021-11-29 15:49:33 +0100

    start

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 
 def main ():
     #start
-    # temp below
-    windows = 1
+    size = width, height = 500, 500
+    screen = pygame.display.set_mode(size)
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
