34ec348bbabb6f2eabd3d5d3a6ba0bde90efef92
Kub-s <Kazio33@hotmail.com>
2021-11-29 15:33:11 +0100

    cool

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 #ur mom gay
-# kub is cube confirmed
-# https://prod.liveshare.vsengsaas.visualstudio.com/join?FB41B256CA3489D1B101F3F35E46C3F631DC
+#kub is cube confirmed
+
+def main ():
+    #start
