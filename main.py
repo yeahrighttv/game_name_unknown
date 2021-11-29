2187708a1df70e86e3bb70a97a8a48caea5ce710
yeahrighttv <martinjansander@gmail.com>
2021-11-29 17:17:20 +0100

    added clolors

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import pygame, sys
-from pygame.locals import *
+import pygame
 
 def main ():
     pygame.init()
 
-    screen = pygame.display.set_mode(500, 500)
-    pygame.display.set_caption('Gyarbete')
 
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
+screen = pygame.display.set_mode(600, 400)
+
 
 
 if __name__ == '__main__':
