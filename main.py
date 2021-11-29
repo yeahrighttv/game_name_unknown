29ee0cb6eb5e91614db98cd5b7a02f8a9bc0ecdc
yeahrighttv <martinjansander@gmail.com>
2021-11-29 15:58:19 +0100

    test

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import pygame, sys
-
-pygame.init()
+from pygame.locals import *
 
 def main ():
-    #start
-    size = width, height = 500, 500
-    screen = pygame.display.set_mode(size)
+    pygame.init()
+
+    screen = pygame.display.set_mode(500, 500)
+    pygame.display.set_caption('Gyarbete')
 
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
 
-        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
