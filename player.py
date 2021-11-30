6c346e5e201e51a2387e5c36eaa5c784bc54fc15
yeahrighttv <martinjansander@gmail.com>
2021-11-30 10:25:52 +0100

    Co-authored-by: Kub-s <Kub-s@users.noreply.github.com>

--- player.py
+++ player.py
@@ -5,6 +5,9 @@ class Player:
     def __init__(self, x_position, y_position):
         print('Player Created')
         self.position = [x_position, y_position]
+        self.image = pygame.image.load('/imgs/player.png')
+        self.image = pygame.transform.scale(self.image, (config.SCALE, config.SCALE))
+        self.rect = pygame.Rect(self.position[0] * config.SCALE, self.position[1] * config.SCALE, config.SCALE, config.SCALE)
 
     def update(self):
         print('Player Updated')
@@ -12,6 +15,7 @@ class Player:
     def update_position(self, x_change, y_change):
         self.position[0] += x_change
         self.position[1] += y_change
+        self.rect = self.position[0] * config.SCALE, self.position[1] * config.SCALE, config.SCALE, config.SCALE
 
     def render(self, screen):
-        pygame.draw.rect(screen, config.WHITE, (self.position[0] * config.SCALE, self.position[1] * config.SCALE, config.SCALE, config.SCALE), 4)
+        screen.blit(self.image, self.rect)
