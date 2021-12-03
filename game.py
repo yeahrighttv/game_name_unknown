23cb5187ef4b3dbd8c7431c233d0176db3898ee5
Kub-s <Kazio33@hotmail.com>
2021-12-03 23:55:28 +0100

    FINALLY GOT SMOOTH MOVEMENT OMG

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-from player import Player, pressedkeys, stepcount
+from player import Player
 import config
 from game_state import GameState
 
@@ -11,7 +11,7 @@ class Game:
         self.game_state = GameState.NONE
 
     def set_up(self):
-        player = Player(1, 1)
+        player = Player(0, 0)
         self.player = player
         self.objects.append(player)
         print('do set up')
@@ -23,7 +23,7 @@ class Game:
         self.screen.fill(config.BLACK)
         print('update')
         self.handle_events()
-
+        self.player.update_position()
         for object in self.objects:
             object.render(self.screen)
 
@@ -31,52 +31,42 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.game_state = GameState.ENDED
-            # hanlde key events
+            # handle key events
             #check if key is pressed
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.game_state = GameState.ENDED
                 elif event.key == pygame.K_w or event.key == pygame.K_UP: # up
                     #self.player.update_position(0, -1)
-                    pressedkeys['up'] = True
+                    self.player.up_pressed = True
                     print("up v")
+                    print(self.player.right_pressed, self.player.left_pressed, self.player.up_pressed, self.player.down_pressed)
                 elif event.key == pygame.K_s or event.key == pygame.K_DOWN: # down
                     #self.player.update_position(0, 1)
-                    pressedkeys['down'] = True
+                    self.player.down_pressed = True
+                    print(self.player.right_pressed, self.player.left_pressed, self.player.up_pressed, self.player.down_pressed)
                     print("down v")
                 elif event.key == pygame.K_a or event.key == pygame.K_LEFT: # left
                     #self.player.update_position(-1, 0)
-                    pressedkeys['left'] = True
+                    self.player.left_pressed = True
+                    print(self.player.right_pressed, self.player.left_pressed, self.player.up_pressed, self.player.down_pressed)
                     print("left v")
                 elif event.key == pygame.K_d or event.key == pygame.K_RIGHT: # right
                     #self.player.update_position(1, 0)
-                    pressedkeys['right'] = True
+                    self.player.right_pressed = True
+                    print(self.player.right_pressed, self.player.left_pressed, self.player.up_pressed, self.player.down_pressed)
                     print("right v")
             #check if key is released
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_w or event.key == pygame.K_UP:
-                    pressedkeys['up'] = False
+                    self.player.up_pressed = False
                     print("up ^")
                 elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    pressedkeys['down'] = False
+                    self.player.down_pressed = False
                     print("down ^")
                 elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    pressedkeys['left'] = False
+                    self.player.left_pressed = False
                     print("left ^")
                 elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    pressedkeys['right'] = False
+                    self.player.right_pressed = False
                     print("right ^")
-            #if key is pressed: change posistion
-            if pressedkeys['up']:
-                pygame.time.wait(50)
-                self.player.update_position(0, -1)
-            if pressedkeys['down']:
-                pygame.time.wait(50)
-                self.player.update_position(0, 1)
-            if pressedkeys['left']:
-                pygame.time.wait(50)
-                self.player.update_position(-1, 0)
-            if pressedkeys['right']:
-                pygame.time.wait(50)
-                self.player.update_position(1, 0)
-#check if keys are pressed
--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from os import spawnle
 import pygame
 import config
 
@@ -7,40 +8,46 @@ walk_down = [pygame.image.load('imgs/player_walk1_down.png'), pygame.image.load(
 walk_left = [pygame.image.load('imgs/player_walk1_left.png'), pygame.image.load('imgs/player_walk2_left.png')]
 walk_right = [pygame.image.load('imgs/player_walk1_right.png'), pygame.image.load('imgs/player_walk2_right.png')]
 
-#check if keys are pressed
-pressedkeys = {
-    'up': False,
-    'down': False,
-    'left': False,
-    'right': False
-}
+
+
 
 stepcount = 0
 
 class Player:
     def __init__(self, x_position, y_position):
         print('Player Created')
-        self.position = [x_position, y_position]
-        if pressedkeys['down']:
-            self.image = walk_down[1]
-        elif pressedkeys['up']:
-            self.image = walk_up[1]
-        elif pressedkeys['left']:
-            self.image = walk_left[1]
-        elif pressedkeys['right']:
-            self.image = walk_right[1]
-        else:
-            self.image = pygame.image.load('imgs/player.png')
+        self.x = int(x_position)
+        self.y = int(y_position)
+        self.velX = 0
+        self.velY = 0
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        self.speed = 0.1
+        self.image = pygame.image.load('imgs/player.png')
         self.image = pygame.transform.scale(self.image, (config.SCALE, config.SCALE))
-        self.rect = pygame.Rect(self.position[0] * config.SCALE, self.position[1] * config.SCALE, config.SCALE, config.SCALE)
+        self.rect = pygame.Rect(self.x * config.SCALE, self.y * config.SCALE, config.SCALE, config.SCALE)
 
     def update(self):
         print('Player Updated')
     
-    def update_position(self, x_change, y_change):
+    def update_position(self):
         global stepcount
-        self.position[0] += x_change
-        self.position[1] += y_change
-        self.rect = self.position[0] * config.SCALE/10, self.position[1] * config.SCALE/10, config.SCALE, config.SCALE
+        self.velX = 0
+        self.velY = 0
+        if self.left_pressed == True and self.right_pressed == False:
+            self.velX = -self.speed
+        if self.right_pressed == True and self.left_pressed == False:
+            self.velX = self.speed
+        if self.up_pressed == True and self.down_pressed == False:
+            self.velY = -self.speed
+        if self.down_pressed == True and self.up_pressed == False:
+            self.velY = self.speed
+        
+        self.x += self.velX
+        self.y += self.velY
+        self.rect = self.x * config.SCALE, self.y * config.SCALE, config.SCALE, config.SCALE
+    
     def render(self, screen):
         screen.blit(self.image, self.rect)
