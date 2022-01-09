b9af2f38a3fbf9cb521adf75eef83a4f574b554f
Dmitry Chirin <chirin.dima@gmail.com>
2022-01-09 18:48:13 +0100

    removed some unused code

--- player.py
+++ player.py
@@ -6,7 +6,6 @@ import config
 
 
 # sprites sorted in lists
-
 walk_up = [pygame.image.load('imgs/player_walk1_up.png'), pygame.image.load('imgs/player_walk2_up.png'), pygame.image.load('imgs/player_walk3_up.png'),  pygame.image.load('imgs/player_walk2_up.png')]
 walk_down = [pygame.image.load('imgs/player_walk1_down.png'), pygame.image.load('imgs/player_walk2_down.png'), pygame.image.load('imgs/player_walk3_down.png'),  pygame.image.load('imgs/player_walk2_down.png')]
 walk_left = [pygame.image.load('imgs/player_walk1_left.png'), pygame.image.load('imgs/player_walk2_left.png')]
@@ -40,9 +39,6 @@ class Player(Sprite):
 
         self.image = pygame.transform.scale(self.image, (config.SCALE, config.SCALE))
         self.rect = pygame.Rect(self.x * config.SCALE, self.y * config.SCALE, config.SCALE, config.SCALE)
-
-    # def update(self):
-    #     print('Player Updated')
 
     def advance_animation(self):
         self.horizontal_animation_counter += 1
@@ -89,20 +85,3 @@ class Player(Sprite):
 
         # rescale movement
         self.rect = pygame.Rect(self.x * config.SCALE, self.y * config.SCALE, config.SCALE, config.SCALE)
-
-    # def render(self, screen):
-    #     bgsurface = pygame.surface.Surface((317, 236))
-    #     entrance = pygame.image.load('imgs/Room_Entrance.png')
-    #     stairs = pygame.image.load('imgs/Staircase_1.png')
-    #     rail1 = pygame.image.load('imgs/Railing_asset1.png')
-    #     rail2 = pygame.image.load('imgs/Railing_asset2.png')
-    #     rail3 = pygame.image.load('imgs/Railing_asset3.png')
-    #
-    #     pygame.Surface.blit(bgsurface, stairs, (0, 0))
-    #     pygame.Surface.blit(bgsurface, entrance, (0, 0))
-    #     bgsurface.blit(self.image, self.rect)
-    #     pygame.Surface.blit(bgsurface, rail1, (0, 0))
-    #     pygame.Surface.blit(bgsurface, rail2, (0, 0))
-    #     pygame.Surface.blit(bgsurface, rail3, (0, 0))
-    #
-    #     pygame.transform.scale(bgsurface, (317 * 3, 236 * 3), dest_surface=screen)
