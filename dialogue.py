2a24b71d38436771df918d4009e51abb749769e0
yeahrighttv <martinjansander@gmail.com>
2022-03-08 09:10:47 +0100

    dialogue class almost done, needs testing

--- dialogue.py
+++ dialogue.py
@@ -14,3 +14,30 @@ class Dialogue(AbstractState):
         self.dialogue_text = dialogue_text
 
         screen_size = self.og_screen_size * self.screen_scaling_factor
+
+        portrait = main_fight_sprite_path.get_rect()
+        portrait_width = int(portrait.x)
+        portrait_height = int(portrait.y)
+
+        self.dialog_box = textboxify.TextBoxFrame(
+            text=dialogue_text,
+            text_width=(screen_size.x // 2),
+            lines=2,
+            pos=((screen_size.x // 8), (screen_size // 2)),
+            padding=(150, 100),
+            font_color=(255, 255, 255),
+            font_size=26,
+            bg_color=(0, 0, 0),
+            border=DEFAULT,
+            alpha=255
+        )
+
+        dialog_box.set_indicator()
+
+        dialog_box.set_portrait(f"imgs/{main_fight_sprite_path}", (portrait_width, portrait_height))
+
+    def set_up(self):
+        self.test_dct = {
+            pygame.K_9: lambda x, y: self.game.change_res(x, y - 1),
+            pygame.K_0: lambda x, y: self.game.change_res(x, y + 1),
+        }
